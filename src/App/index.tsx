56a41f271f09a847082ef34cc20573a537675ac3
Visualizeit <17688172112@163.com>
2022-10-13 22:04:15 +0800

    feat: add react-query

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,21 @@
 import { MantineProvider } from '@mantine/core'
 import { RouterProvider } from 'react-router-dom'
 import { Talkr } from 'talkr'
+import { QueryClientProvider } from '@tanstack/react-query'
 import mantineTheme from 'App/config/mantineTheme'
 import router from 'App/router'
 import languages from './i18n/languages'
+import queryClient from './config/queryClient'
 
 const App = () => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
-      <Talkr languages={languages} defaultLanguage="en_us">
-        <RouterProvider router={router} />
-      </Talkr>
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
+        <Talkr languages={languages} defaultLanguage="en_us">
+          <RouterProvider router={router} />
+        </Talkr>
+      </MantineProvider>
+    </QueryClientProvider>
   )
 }
 
