f0fa4f01f5617d7e3bcef9777b5caaa57057ac09
Visualizeit <17688172112@163.com>
2022-10-25 22:19:14 +0800

    feat: add FilesList Animate

--- src/App/features/Files/components/FilesList.tsx
+++ src/App/features/Files/components/FilesList.tsx
@@ -1,8 +1,15 @@
 import { type PropsWithChildren } from 'react'
 import { List } from '@mantine/core'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 const FilesList = ({ children }: PropsWithChildren<unknown>) => {
-  return <List className="flex gap-md">{children}</List>
+  const [parent] = useAutoAnimate<HTMLUListElement>()
+
+  return (
+    <List ref={parent} className="flex gap-md">
+      {children}
+    </List>
+  )
 }
 
 export default FilesList
