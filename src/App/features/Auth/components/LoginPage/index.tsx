314f3d7536da7423ffcf133fa6a8b8d203366c5c
Visualizeit <17688172112@163.com>
2022-10-28 23:41:38 +0800

    fix: LoginPage route animation shake

--- src/App/features/Auth/components/LoginPage/index.tsx
+++ src/App/features/Auth/components/LoginPage/index.tsx
@@ -13,34 +13,36 @@ const LoginPage = () => {
   const { form, handleSubmit, isLoading } = useLoginForm()
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <Title className="mb-12" order={2} align="center" mt="md">
         Welcome back to Finest Map!
       </Title>
-      <TextInput
-        type="email"
-        label="Email"
-        placeholder="hello@gmail.com"
-        size="md"
-        {...form.getInputProps('email')}
-      />
-      <PasswordInput
-        label="Password"
-        placeholder="Your password"
-        mt="md"
-        size="md"
-        {...form.getInputProps('password')}
-      />
-      <Button type="submit" fullWidth mt="xl" size="md" loading={isLoading}>
-        Login
-      </Button>
-      <Text color="gray" align="center" mt="xl">
-        Don&apos;t have an account?{' '}
-        <Anchor component={Link} to="/sign_up" weight="bold">
-          Sign Up for free
-        </Anchor>
-      </Text>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          type="email"
+          label="Email"
+          placeholder="hello@gmail.com"
+          size="md"
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          mt="md"
+          size="md"
+          {...form.getInputProps('password')}
+        />
+        <Button type="submit" fullWidth mt="xl" size="md" loading={isLoading}>
+          Login
+        </Button>
+        <Text color="gray" align="center" mt="xl">
+          Don&apos;t have an account?{' '}
+          <Anchor component={Link} to="/sign_up" weight="bold">
+            Sign Up for free
+          </Anchor>
+        </Text>
+      </form>
+    </>
   )
 }
 
--- src/App/features/Auth/components/SignUpPage/index.tsx
+++ src/App/features/Auth/components/SignUpPage/index.tsx
@@ -13,45 +13,48 @@ const SignUpPage = () => {
   const { form, handleSubmit, isLoading } = useSignUpForm()
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <Title className="mb-12" order={2} align="center" mt="md">
         Create new account.
       </Title>
-      <TextInput
-        label="Name"
-        placeholder="Your Name"
-        required
-        size="md"
-        {...form.getInputProps('name')}
-      />
-      <TextInput
-        type="email"
-        label="Email"
-        placeholder="hello@gmail.com"
-        required
-        mt="md"
-        size="md"
-        {...form.getInputProps('email')}
-      />
-      <PasswordInput
-        label="Password"
-        placeholder="Your password"
-        required
-        mt="md"
-        size="md"
-        {...form.getInputProps('password')}
-      />
-      <Button type="submit" fullWidth mt="xl" size="md" loading={isLoading}>
-        Sign Up
-      </Button>
 
-      <Text color="gray" align="center" mt="xl">
-        Already have an account?{' '}
-        <Anchor component={Link} to="/login" weight="bold">
-          Log In
-        </Anchor>
-      </Text>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          label="Name"
+          placeholder="Your Name"
+          required
+          size="md"
+          {...form.getInputProps('name')}
+        />
+        <TextInput
+          type="email"
+          label="Email"
+          placeholder="hello@gmail.com"
+          required
+          mt="md"
+          size="md"
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+          size="md"
+          {...form.getInputProps('password')}
+        />
+        <Button type="submit" fullWidth mt="xl" size="md" loading={isLoading}>
+          Sign Up
+        </Button>
+
+        <Text color="gray" align="center" mt="xl">
+          Already have an account?{' '}
+          <Anchor component={Link} to="/login" weight="bold">
+            Log In
+          </Anchor>
+        </Text>
+      </form>
+    </>
   )
 }
 
