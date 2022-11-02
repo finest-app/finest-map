40570c9a43d4d84b3808083d645a7384538c4807
Visualizeit <17688172112@163.com>
2022-11-02 21:17:46 +0800

    feat: Add translation of AppNavBar

--- src/App/features/Root/components/AppNavBar/index.tsx
+++ src/App/features/Root/components/AppNavBar/index.tsx
@@ -2,6 +2,7 @@ import { Navbar, Stack } from '@mantine/core'
 import { IconFiles, IconUser, IconLogout, IconSettings } from '@tabler/icons'
 import { useLogout } from 'App/features/Auth/api'
 import useIsLoggedIn from 'App/features/Auth/hooks/useIsLoggedIn'
+import { useAutocompleteT } from 'App/i18n'
 import NavbarButton from './NavbarButton'
 import NavbarLink from './NavbarLink'
 
@@ -9,16 +10,24 @@ const AppNavBar = () => {
   const isLoggedIn = useIsLoggedIn()
   const logoutMutation = useLogout()
 
+  const { T } = useAutocompleteT()
+
   return (
-    <Navbar width={{ base: 64 }} p="xs">
+    <Navbar className="justify-between" width={{ base: 64 }} px="xs" py="md">
       {/* Navbar content */}
       <Navbar.Section component={Stack}>
-        <NavbarLink label="Files" to="/files" icon={IconFiles} />
-        <NavbarLink label="Account" to="/account" icon={IconUser} />
-        <NavbarLink label="Settings" to="/settings" icon={IconSettings} />
+        <NavbarLink label={T('navbar.files')} to="/files" icon={IconFiles} />
+        <NavbarLink label={T('navbar.account')} to="/account" icon={IconUser} />
+        <NavbarLink
+          label={T('navbar.settings')}
+          to="/settings"
+          icon={IconSettings}
+        />
+      </Navbar.Section>
+      <Navbar.Section component={Stack}>
         {isLoggedIn && (
           <NavbarButton
-            label="Logout"
+            label={T('navbar.logout')}
             icon={IconLogout}
             onClick={() => {
               logoutMutation.mutate()
